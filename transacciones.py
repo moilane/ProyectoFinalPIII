46904ef6aeda82dbefef418bfd7ac0a4e6d9c174
MCAlmond <31973758+MCAlmond@users.noreply.github.com>
2018-04-24 23:06:10 -0500

    update 1.2
    adicion al menu, necesita trabajar en el funcionamiento del menu

--- transacciones.py
+++ transacciones.py
@@ -1,38 +1,101 @@
 
 import os
 import time
+from collections import OrderedDict
 
-transaccion = {}
+#El menu funciona bien y se guarda en el .txt, lo subo junto para que vean como funciona, pero aun se tiene que aplicar las funcionas en las opciones.
+# y no se coloca el mensaje en si en la opcion, puede pobarlo cuando ejecuta el problema.
+#y el loop, un error cuando el cliente no coloca bien la opcion y poner el ID del empleado y el precio que seria el monto
+#el problema con el programa es la manera que se guarda el .txt, no se puede guarda normalmente ya que el terminal da un error por el string
+# Este es el error:    
+#  file.write(transaccion + '\n')
+# TypeError: unsupported operand type(s) for +: 'dict' and 'str'
+#En la cual lo tenia que se reemplazada con el codigo escrito abajo en la linea 95 respectivamente. 
 
-def add_initials(nom, ape):
+def opcion_1():
+    """Efectivo"""
+    print("Seleccionaste Efectivo!\n")
+    print("Porfavor ingrese la cantidad desada que quieres pagar:")
+
+
+def opcion_2():
+    """Tarjeta de Debito/Credito"""
+    print("Seleccionaste Tarjeta de Debito/Credito!\n")
+    print("Porfavor ingrese los 16 digitos de su tarjeta: ")
+    print("Fecha de expiracion: ")
+
+os.system('cls')
+
+if __name__ == '__main__':
+  transaccion = {}
+
+def add_initials(nom, ape, time, id):
     add = {}
 
+    add["time"] = time
+    add["ID de Empleado"] = id
     add["nom"] = nom
     add["ape"] = ape
+    
+    transaccion[id] = add
 
-    transaccion[nom] = add
+os.system('cls')
 
 def fractura():
-    for nom in transaccion:
-        add = transaccion[nom]
-        print(time.strftime("%c"), "\n", add["nom"], "\n", add["ape"], "\n", "Forma de pago escogido: ", "Monto: ", "Pago: ")
+    for id in transaccion:
+        add = transaccion[id]
+        print(add["time"], "\n", add["ID de Empleado"], "\n", add["nom"], "\n", add["ape"], "\n", "Forma de pago escogido: ", "Monto: ", "Pago: ")
+
+print("¡Bienvenido a la pantalla de transacciones de Bomba iPlena! Porfavor siga las instrucciones para procesa su transaccion.\n")
 
 def add2():
+    id = input("Ingrese su ID de Empleado: ")
     n = input("Nombre: ")
     a = input("Apellido: ")
+    t = time.strftime("%m/%d/%Y, %H:%M:%S")
 
-    add_initials(n, a)
+    add_initials(n, a, t, id)
 
     fractura()
+add2()  
 
-add2()
+os.system('cls')
 
+salir = False
+mensaje = ('Porfavor seleccione su metodo de pago para procesar su transaccion.')
+
+    # menu = {'1' : opcion_1, '2' : opcion_2 }
+
+menu = OrderedDict(
+        [
+            ('1', opcion_1),
+            ('2', opcion_2),
+        ]
+    )
+
+while not salir:
+    print(mensaje)
+
+    for funcion, opcion in menu.items():
+        mensaje_final = '{}) {}'.format(funcion, opcion.__doc__)
+        print(mensaje_final)
+    
+    respuesta = input('\nOpcion: ')
+    salir = respuesta == '0'
+
+    opcion = menu.get(respuesta, None)
+    if opcion:
+        opcion()
+
+else:
+    print("Gracias por su transaccion! Tenga un buen dia!")
+            
 
 ruta2 = '.vscode\\transaccion.txt'
 file = open(ruta2,'a+')
 file.write('transaccion = % s'%transaccion + '\n')
 file.close()
-print('Archivo escrito')
+print('Transaccion guardada en archivo')
 
 # print('¡Bienvenido a la pantalla de transacciones de Bomba iPlena! Porfavor siga las instrucciones para procesa su transaccion.')
     
